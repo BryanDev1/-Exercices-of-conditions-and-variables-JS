193f3dec50307dc12bbe2d1543c7274ac5f0d1cb
BryanDev1 <bolangebryan@gmail.com>
2024-10-04 12:11:09 +0200

    Adding html and script for the exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+// Declare and define three variables using a numerical value
+
+// * If the three variables are equal, display "The three variables are the same" in the console
+// * If only two of the variables are equal, display "Two of the variables are the same" in the console
+// * If the variables are all different display "all variables are different" in the console
+
+const a = 1;
+const b = 2;
+const c = 3;
+
+if (a === b && b=== c);{
+    console.log("The three variables are the same");
+}
+
+if ( a === b || b === c || c === a );{
+    console.log("Two of the variables are the same");
+
+}
+
+if ( a === b && b === c && c === a );{
+    console.log("all variables are different");
+
+}
+
